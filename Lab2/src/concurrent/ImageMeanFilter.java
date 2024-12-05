65bf9983bb67a1f4240a33c4275e3d7ad367a984
FLuiz22 <filipeluiz39@gmail.com>
2024-12-05 17:49:54 -0300

    tentamo

--- Lab2/src/concurrent/ImageMeanFilter.java
+++ Lab2/src/concurrent/ImageMeanFilter.java
@@ -1,6 +1,9 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 
 /**
@@ -17,7 +20,7 @@ import javax.imageio.ImageIO;
  * 
  * <p>Supported image formats: JPG, PNG</p>
  * 
- * <p>Author: temmanuel@comptuacao.ufcg.edu.br</p>
+ * <p>Author: temmanuel@comptuacao, BufferedImage filBufferedImage.ufcg.edu.br</p>
  */
 public class ImageMeanFilter {
     
@@ -29,9 +32,10 @@ public class ImageMeanFilter {
      * @param kernelSize Size of mean kernel
      * @throws IOException If there is an error reading/writing
      */
-    public static void applyMeanFilter(String inputPath, String outputPath, int kernelSize) throws IOException {
+    public static void applyMeanFilter(String inputPath, String outputPath, int kernelSize, int nThreads) throws IOException, InterruptedException {
         // Load image
         BufferedImage originalImage = ImageIO.read(new File(inputPath));
+        List<Thread> threads = new ArrayList<>();
         
         // Create result image
         BufferedImage filteredImage = new BufferedImage(
@@ -45,7 +49,95 @@ public class ImageMeanFilter {
         int height = originalImage.getHeight();
         // Process each pixel
         for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x+=nThreads) {
+                List<Task> ts = new ArrayList<>();
+                // Calculate neighborhood average
+                /* int[] avgColor = calculateNeighborhoodAverage(originalImage, x, y, kernelSize);
+                
+                // Set filtered pixel
+                filteredImage.setRGB(x, y, 
+                    (avgColor[0] << 16) | 
+                    (avgColor[1] << 8)  | 
+                    avgColor[2]
+                ); */
+
+                for (int i = 0; i < nThreads; i++) {
+                    Task task = new Task(originalImage, x, y, kernelSize);
+
+                    Thread thread = new Thread(task);
+                    ts.add(task);
+                    thread.start();
+                }
+
+
+
+                for (int i = 0; i < ts.size(); i++) {
+                    int[] avgColor = ts.get(i).getOut();
+                    
+                    filteredImage.setRGB(x+i, y, 
+                        (avgColor[0] << 16) | 
+                        (avgColor[1] << 8)  | 
+                        avgColor[2]
+                    ); 
+                }
+            }
+        }
+
+
+
+        
+        // Save filtered image
+        ImageIO.write(filteredImage, "jpg", new File(outputPath));
+    }
+
+    //abordagem 1
+
+    public static class Task implements Runnable {
+        private final BufferedImage originalImage;
+        private int x;
+        private int y;
+        private int kernelSize;
+        private int[] out;
+
+        public Task(BufferedImage originalImage, int x, int y, int kernelSize) {
+            this.originalImage = originalImage;
+            this.x = x;
+            this.y = y;
+            this.kernelSize = kernelSize;
+        }
+
+        @Override
+        public void run() {
+            int[] avgColor = calculateNeighborhoodAverage(originalImage, x, y, kernelSize);
+                
+            out = avgColor;
+        }
+
+        public int[] getOut() {
+            return out;
+        }
+    }
+
+    //abordagem 2
+
+    public static class Task2 implements Runnable {
+        private final BufferedImage originalImage;
+        private int width;
+        private int y;
+        private int kernelSize;
+        private int[] out;
+
+        public Task2(BufferedImage originalImage, int width, int y, int kernelSize) {
+            this.originalImage = originalImage;
+            this.width = width;
+            this.y = y;
+            this.kernelSize = kernelSize;
+        }
+
+        @Override
+        public void run() {
             for (int x = 0; x < width; x++) {
+                List<Task> ts = new ArrayList<>();
                 // Calculate neighborhood average
                 int[] avgColor = calculateNeighborhoodAverage(originalImage, x, y, kernelSize);
                 
@@ -56,10 +148,12 @@ public class ImageMeanFilter {
                     avgColor[2]
                 );
             }
+
         }
-        
-        // Save filtered image
-        ImageIO.write(filteredImage, "jpg", new File(outputPath));
+
+        public int[] getOut() {
+            return out;
+        }
     }
     
     /**
@@ -135,10 +229,13 @@ public class ImageMeanFilter {
         }
 
         String inputFile = args[0];
+        int nThreads = new Integer(args[1]);
         try {
-            applyMeanFilter(inputFile, "filtered_output.jpg", 7);
+            applyMeanFilter(inputFile, "filtered_output.jpg", 7, nThreads);
         } catch (IOException e) {
             System.err.println("Error processing image: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("F");
         }
     }
 }
