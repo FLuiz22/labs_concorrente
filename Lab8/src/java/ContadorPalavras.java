40f0018cbcb577b6a2f309e0bbec537546ba498a
FLuiz22 <filipeluiz39@gmail.com>
2025-03-28 08:54:09 -0300

    Feat: Lab8 ContadorPalavras1

---
+++ Lab8/src/java/ContadorPalavras.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.concurrent.*;
+
+public class ContadorPalavras {
+    public static void main(String[] args) throws IOException {
+	System.out.println("Lab8");
+
+    int count = 0;
+
+    for(String arg : args) {
+        count += contarPalavras(arg);
+    }
+
+    System.out.println("Soma de palavras: " + count);
+
+    }
+
+    static int contarPalavras(String nomeArquivo) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(nomeArquivo));
+        int count = 0;
+        String linha;
+        while ((linha = br.readLine()) != null) {
+            count += linha.split("\\s+").length;
+        }
+        br.close();
+        return count;
+    }
+}
